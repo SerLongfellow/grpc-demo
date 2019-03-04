400ee8938c36759232821e855d576b1d5b9456d4
Jeff Long <jlong@privacystar.com>
2019-03-04 09:26:26 -0800

    Added HTTP 1.1 client

--- client/src/epoch_conversion_client.py
+++ client/src/epoch_conversion_client.py
@@ -1,29 +1,75 @@
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import parse
+
 import grpc
 
 import epoch_converter_service_pb2
 import epoch_converter_service_pb2_grpc
 
 
+grpc_channel = grpc.insecure_channel('localhost:5000')
+
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        return
+
+    def do_POST(self):
+        return
+
+    def do_GET(self):
+        print("Got GET request!")
+
+        ds = parse.parse_qs(parse.urlparse(self.path).query).get("ds", None)
+        ds = ds[0]
+
+        stub = epoch_converter_service_pb2_grpc.EpochConverterServiceStub(grpc_channel)
+        epoch = make_request(stub, ds)
+        response = "Datetime conversion result is " + epoch
+
+        self.send_response(200)
+        self.send_header("content-type", "application/text")
+        self.end_headers()
+        self.wfile.write(bytes("{}\n".format(response), "UTF-8"))
+
+    def handle_http(self):
+        return
+
+    def respond(self):
+        return
+
+
 def main():
-    with grpc.insecure_channel('localhost:5000') as channel:
-        stub = epoch_converter_service_pb2_grpc.EpochConverterServiceStub(channel)
 
-        ds = "2019-02-25 15:33:01"
+    print("Starting server...")
+    httpd = HTTPServer(("0.0.0.0", 8080), HttpHandler)
 
-        make_request(stub, ds)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Shutting down...")
 
-        ds = "not a real date"
-        make_request(stub, ds)
+    httpd.server_close()
 
 
 def make_request(stub, ds):
     try:
-        print("Making RPC request with date string: " + ds)
-        response = stub.ConvertDateToEpochSeconds(epoch_converter_service_pb2.ConversionRequest(date_string=ds))
+        request = epoch_converter_service_pb2.ConversionRequest(date_string=ds)
+
+        print("Making RPC request: " + str(request))
+        response = stub.ConvertDateToEpochSeconds(request)
 
         print("Received response!:\n" + str(response))
+
+        response = str(response.epoch_seconds)
+
     except grpc.RpcError as e:
         print("Error making RPC call:\n{}: {}\n".format(str(e.code()), e.details()))
+
+        response = "Error!"
+
+    return response
 
 
 if __name__ == "__main__":
